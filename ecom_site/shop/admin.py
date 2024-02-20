ff2d4aaca71caf62cd4dad6c07a69f2924a9b5b2
SHARMA-ANK <ankitsharma_22248@aitpunr.edu.in>
2024-02-20 16:14:03 +0530

    Last Thing

--- ecom_site/shop/admin.py
+++ ecom_site/shop/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
 from .models import Products,Order
+admin.site.site_header="E-commerce Site"
+admin.site.site_title="Ankit Shopping"
+admin.site.index_title="Manage Ankit Shopping"
 # Register your models here.
-admin.site.register(Products)
+
+class ProductAdmin(admin.ModelAdmin):
+    def change_category_to_default(self,request,queryset):
+        queryset.update(category="default")
+    change_category_to_default.short_description='Default Category'
+    list_display=('title','price','discount_price','description','category')
+    search_fields=('title','category')
+    actions=('change_category_to_default',)
+    fields=('title','price')
+    list_editable=('price','discount_price','category')
+admin.site.register(Products,ProductAdmin)
 admin.site.register(Order)
